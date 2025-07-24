040f015a194be530f57ff7f0c57a83b96e2c2343
Diogo Portela de Oliveira <diogo.oliveira@rommanel.com.br>
2025-07-24 14:32:01 -0300

    Remoção do TOKEN

--- run.bat
+++ run.bat
@@ -1,7 +1,11 @@
 docker compose down
+timeout /t 1
 docker compose up --build -d
+timeout /t 1
 docker compose ps
+timeout /t 1
 docker ps -a
+timeout /t 20
 .\keycloak8080.url
 .\rabbitmq15672.url
 .\frontend5173.url
